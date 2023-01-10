c2ee4dd446b09e853edd2ae4ec237aaa1ef11e64
sandeepcnath <sandeepcnath@gmail.com>
2023-01-10 11:57:26 +0530

    gatsby config updated

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,13 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `DM Living`,
+    author: {
+      name: `sandeep c nath`,
+    },
+    siteUrl: `https://dm-living.netlify.com/`,
+  },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
